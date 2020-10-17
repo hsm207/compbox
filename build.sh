38246f0f8e63e0a3eb3e16d292ad2703b950e36e
Ubuntu <ubuntu@ip-172-31-67-68.ec2.internal>
2020-10-17 15:47:44 +0000

    Fix typo

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 docker login -u $1 -p $2
-docker build -t $DOCKERHUB_USERNAME/compbox . && \
-    docker push $DOCKERHUB_USERNAME/compbox:latest
+docker build -t $1/compbox . && \
+    docker push $1/compbox:latest
